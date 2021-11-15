f2459ed12c119014eff8d9c216fb2f4fe050c0bf
Noa Udler <udlernoa4@gmail.com>
2021-11-14 22:02:50 -0800

    trying again

--- src/index.js
+++ src/index.js
@@ -1,58 +1,47 @@
-//Get images from server and add contents of object to DOM
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000/images")
-    .then((response) => response.json())
-    .then((imageElements) =>
-      imageElements.forEach((imageElement) => {
-        const imageTitle = document.querySelector("#card-title");
-        imageTitle.innerText = imageElement.title;
-        const image = document.querySelector("#card-image");
-        image.setAttribute("src", imageElement.image);
-        const likeCount = document.querySelector("#like-count");
-        likeCount.innerText = `${imageElement.likes} likes`;
-        //Adding click event and listener on heart like button to increase likes whenever clicked
-        const likeButton = document.querySelector("#like-button");
-        likeButton.addEventListener("click", (event) =>
-          handleHeartClick(event, imageElement.likes)
-        );
-      })
-    );
-   // Getting comments and removing current li's on html and replacing them with comments from the server 
-  fetch("http://localhost:3000/comments")
-    .then((response) => response.json())
-    .then((comments) => {
-      const ulContainer = document.querySelector("#comments-list");
-      const firstLi = document.querySelector('#first-li')
-      const secondLi = document.querySelector('#second-li')
-      const thirdLi = document.querySelector('#third-li')
-      firstLi.remove()
-      secondLi.remove()
-      thirdLi.remove()
-      comments.forEach((comment) => {
-        const li = document.createElement("li");
-        li.innerText = comment.content;
-        ulContainer.appendChild(li);
-      });
-    });
-});
+fetch('http://localhost:3000/images/1')
+.then(resp => resp.json())
+.then(renderImage)
 
-//Function to handle heart click like button event
-function handleHeartClick(event, imageLikes) {
-  const likeCount = document.querySelector("#like-count");
-  likeCount.innerHTML = `${imageLikes + 1} likes`;
+function renderImage (image) {
+  const h2 = getEl('card-title')
+  h2.textContent = image.title
+  const cardImage = getEl('card-image')
+  cardImage.src = image.image
+  const likeCount = getEl('like-count')
+  likeCount.textContent = `${image.likes} likes`
+  const commentContainer = getEl('comments-list')
+  commentContainer.innerHTML = ''
+  const firstLi = createElement('li')
+  firstLi.innerText = image.comments[0].content
+  const secondLi = createElement('li')
+  secondLi.innerText = image.comments[1].content
+  const thirdLi = createElement('li')
+  thirdLi.innerText = image.comments[2].content
+  commentContainer.append(firstLi,secondLi,thirdLi)
+  const likeButton = getEl('like-button')
+  likeButton.addEventListener('click', () => incrementLikes(likeCount, image.likes))
+  const postButton = document.querySelector('.comment-button')
+  postButton.addEventListener('click', () => handleCommentClick(event, commentContainer))
 }
 
-//Adding click event and listener to post button 
-const postButton = document.querySelector(".comment-button");
-postButton.addEventListener("click", handlePostClick);
+function handleCommentClick (event, commentList) {
+  event.preventDefault()
+  const commentText = getEl('comment').value
+  commentList.append(commentText)
+  document.getElementById('comment').value = ''
+}
 
-//Function to handle adding comments and them posting to the comment board when post button is clicked
-function handlePostClick(event) {
-  event.preventDefault();
-  const commentValue = document.querySelector(".comment-input").value;
-  const ulContainer = document.querySelector("#comments-list");
-  const li = document.createElement("li");
-  li.innerText = commentValue;
-  ulContainer.appendChild(li);
-  document.querySelector(".comment-input").value = ''
+let count = 0
+function incrementLikes (likeCount, likes) {
+count += likes + 1
+console.log(count)
+likeCount.innerText = `${count} likes`
+}
+
+function getEl(id) {
+  return document.getElementById(id)
+}
+
+function createElement(element) {
+  return document.createElement(element)
 }
